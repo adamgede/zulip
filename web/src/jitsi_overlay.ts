5fc1b00a86d8f34e0010b92f165e5d5e94a05a32
Adam Gedeborg <adamthestudly@gmail.com>
2024-07-19 20:07:23 -0600

    Updated view to provide a download link for the Dragon Chat app.

--- web/src/jitsi_overlay.ts
+++ web/src/jitsi_overlay.ts
@@ -5,15 +5,15 @@ import render_jitsi_overlay from "../templates/jitsi_overlay.hbs";
 import * as overlays from "./overlays";
 
 export function initialize(): void {
-    let facebookDeep = 'jitsi-meet://'; // Deep Link Application URL protocol.
-    let facebookSecure = 'https://'; // Regular secure HTTPS URL protocol.
+    let schemeDeep = 'jitsi-meet://'; // Deep Link Application URL protocol.
+    let schemeSecure = 'https://'; // Regular secure HTTPS URL protocol.
 
     $(document).on('click', '[href*="jitsi-meet"]:not(.jitsi-overlay-url)', function(aEvent: MouseEvent) { // Only listen to a tags that have jitsi-meet on them, but are not the overlay urls.
         aEvent.preventDefault(); // Don't navigate with these links (yet).
 
         if (aEvent.currentTarget != null) { // Only show if the target is populated.
             let appUrl = (aEvent.currentTarget as HTMLAnchorElement).href; // Get the "Application" url from the clicked link.
-            let webUrl = appUrl.replace(facebookDeep, facebookSecure); // Replace the "Application" protocol with the "Web" secure protocol.
+            let webUrl = appUrl.replace(schemeDeep, schemeSecure); // Replace the "Application" protocol with the "Web" secure protocol.
             $('#jitsi-app').attr('href', appUrl); // Update the Application link in the overlay.
             $('#jitsi-web').attr('href', webUrl); // Update the Web link in the overlay.
 
