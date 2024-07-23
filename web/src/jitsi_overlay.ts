f1a65855b97d5d6900bceca6640544ee5b887d7c
Adam Gedeborg <adamthestudly@gmail.com>
2024-07-22 19:13:03 -0600

    Update/Fix Jitsi URL generation to also add a JWT token.

--- web/src/jitsi_overlay.ts
+++ web/src/jitsi_overlay.ts
@@ -12,33 +12,26 @@ export function initialize(): void {
         aEvent.preventDefault(); // Don't navigate with these links (yet).
 
         if (aEvent.currentTarget != null) { // Only show if the target is populated.
-            let originalUrl = (aEvent.currentTarget as HTMLAnchorElement).href; // Get the original link to be used later.
+            let appUrl = (aEvent.currentTarget as HTMLAnchorElement).href; // Get the original link to be used later.    
+            let webUrl = appUrl.replace(schemeDeep, schemeSecure); // Replace the "Application" protocol with the "Web" secure protocol.
+            $('#jitsi-app').attr('href', appUrl); // Update the Application link in the overlay.
+            $('#jitsi-web').attr('href', webUrl); // Update the Web link in the overlay.
 
-            fetch('accounts/login/generatejwt/').then((aResponse: Response) => {
-                aResponse.json().then((aValue: any) => {
-                    let token = aValue.token; // Get token from the json response.
-                    let appUrl = originalUrl + "&jwt=" + token; // Using the "Application" url from the clicked link, add on jwt token.
-                    let webUrl = appUrl.replace(schemeDeep, schemeSecure); // Replace the "Application" protocol with the "Web" secure protocol.
-                    $('#jitsi-app').attr('href', appUrl); // Update the Application link in the overlay.
-                    $('#jitsi-web').attr('href', webUrl); // Update the Web link in the overlay.
-
-                    if (overlays.any_active()) {
-                        window.open(appUrl); // When in drafts, open the app url externally instead of showing the popup.
-                    } else {
-                        // Typical processing is to show the overlay where you can click either the app or web popup.
-                        overlays.open_overlay({
-                            name: "jitsi-overlay",
-                            $overlay: $("#jitsi-overlay"),
-                            on_close() {
-                                
-                            },
-                        });
-                    }
+            if (overlays.any_active()) {
+                window.open(appUrl); // When in drafts, open the app url externally instead of showing the popup.
+            } else {
+                // Typical processing is to show the overlay where you can click either the app or web popup.
+                overlays.open_overlay({
+                    name: "jitsi-overlay",
+                    $overlay: $("#jitsi-overlay"),
+                    on_close() {
+                        
+                    },
                 });
-            });
+            }
         }
     });
 
-    const rendered_jitsi_overlay = render_jitsi_overlay({});
-    $("#jitsi-overlay-modal-container").append(rendered_jitsi_overlay);
+    const rendered_jitsi_overlay = render_jitsi_overlay({}); // Render the overlay html.
+    $("#jitsi-overlay-modal-container").append(rendered_jitsi_overlay); // Put the overlay htmel on the page, ready for future use.
 }
